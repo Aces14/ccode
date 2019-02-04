573583875db4ecc286548bd5df698de73824e804
Aces14 <=>
2019-02-04 13:54:28 -0800

    helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+// hello.c
+// compile gcc hello.c
+// run ./a.out
+#include <stdio.h>
+int main(){
+  printf("*** Hello World ***\n");
+  printf("*       from      *\n");
+  printf("*  JJJ C   C   AAA*\n");
+  printf("* J      C C C  A *\n");
+  printf("*******************\n");
+  return 0;
+}
